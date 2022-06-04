b15f85f7a6820cb2a975c6c4a933ee0c229fa482
Nick <Nick_II2000@mail.ru>
2022-06-04 20:49:32 +0600

    12-th commit

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ const btns = document.querySelectorAll('button');
 // }
 
 btns[0].addEventListener('click', () => {
-    if (!btns[1].classList.contains('red')) {
-        btns[1].classList.add('red');
-    } else {
-        btns[1].classList.remove('red');
-    }
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+    btns[1].classList.toggle('red');
 });
