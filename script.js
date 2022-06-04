3987a81d887880a67289d13709fd5a55724c6063
Nick <Nick_II2000@mail.ru>
2022-06-04 20:25:40 +0600

    6-th commit

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const btns = document.querySelectorAll('button');
 console.log(btns[0].classList.add('red', 'brown'));
 // console.log(btns[0].classList.remove('blue'));
 console.log(btns[0].classList.toggle('blue'));
+
+if (btns[1].classList.contains('red')) {
+    console.log('red');
+}
