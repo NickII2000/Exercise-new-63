2861c3822c9a7e55b8f48463faed4067801cc707
Nick <Nick_II2000@mail.ru>
2022-06-04 20:48:27 +0600

    11-th commit

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const btns = document.querySelectorAll('button');
-console.log(btns[0].classList.length);
-console.log(btns[0].classList.item(0));
-console.log(btns[0].classList.add('red', 'brown'));
-console.log(btns[0].classList.remove('blue'));
-console.log(btns[0].classList.toggle('blue'));
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+// console.log(btns[0].classList.add('red', 'brown'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
 
-console.log(btns[1].classList.add('red'));
-if (btns[1].classList.contains('red')) {
-    console.log('red');
-} else {
-    console.log('button has not red');
-}
+// console.log(btns[1].classList.add('red'));
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// } else {
+//     console.log('button has not red');
+// }
+
+btns[0].addEventListener('click', () => {
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red');
+    } else {
+        btns[1].classList.remove('red');
+    }
+});
